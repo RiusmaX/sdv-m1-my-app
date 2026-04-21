0ef44cc4ce771b26c3f8a127f8450625e36a4472
Marius <marius.sergent@gmail.com>
2026-04-21 15:07:06 +0200

    add action buttons

--- components/photo-view.tsx
+++ components/photo-view.tsx
@@ -1,5 +1,6 @@
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import { Image } from "expo-image";
-import { View } from "react-native";
+import { Pressable, View } from "react-native";
 
 function PhotoView ({
     uri,
@@ -14,8 +15,42 @@ function PhotoView ({
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Image 
         source={{ uri }} 
-        style={{ width: "100%", height: "100%", marginBottom: 16 }} 
+        style={{ width: "100%", height: "100%"}} 
       />
+      <Pressable 
+        onPress={onCancel}
+        style={{ 
+          position: "absolute",
+          alignItems: "center",
+          justifyContent: "center",
+          width: 64,
+          height: 64,
+          zIndex: 10,
+          bottom: 36,
+          left: 36,
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+          borderRadius: 999,
+        }}
+      >
+        <MaterialIcons name="cancel" size={24} color="black" />
+      </Pressable>
+       <Pressable 
+        onPress={onValidate}
+        style={{ 
+          position: "absolute",
+          alignItems: "center",
+          justifyContent: "center",
+          width: 64,
+          height: 64,
+          zIndex: 10,
+          bottom: 36,
+          right: 36,
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+          borderRadius: 999,
+        }}
+      >
+        <MaterialIcons name="check" size={24} color="black" />
+      </Pressable>
     </View>
   );
 }
